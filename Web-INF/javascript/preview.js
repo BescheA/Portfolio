d2e4046caabe96a64927d65a1b5a60cf681b7ce1
BescheA <91192238+BescheA@users.noreply.github.com>
2025-05-02 12:43:35 +0200

    Added some files

---
+++ Web-INF/javascript/preview.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a13e0308d9ca42338c7dfa9fc865d22e80b6344c82f80f160045d7582a474da8
+size 1635
--- Web-INF/javascript/scroller.js
+++ Web-INF/javascript/scroller.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55cb91807d8a1f06d2202015a8a8e4d63ae4fa1a64292e692cea7af7bdc3da2f
-size 920
+oid sha256:861a08eba9624a791b876924551b02732d5b55b2397e0a015c3e0b861db7b57a
+size 919
