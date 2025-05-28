ecbfee86ac2a22013eee943feb2a8eedcb6b6579
BescheA <91192238+BescheA@users.noreply.github.com>
2025-05-28 23:55:32 +0200

    Redesigned the webpage

---
+++ UnityProjects/Sadame/resources/web-inf/project.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a0303acd01e8fc646555fd5e48f5955f6596563338a2adce26bcc16429a3bfb6
+size 2477
---
+++ Web-INF/javascript/main.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b92a63dde284c2f8a6a8259eebd59121daba108b2e546757a2ba06357a500c01
+size 4835
